bc0d4092d7af4a4129d4ee1bba27c731221d41ba
Steven Harper <shwesh279@gmail.com>
2018-05-23 12:35:13 -0700

    added through command line

---
+++ index.php
@@ -0,0 +1,77 @@
+
+tas.tritech.io
+<html><head><title>tas.io</title>
+<style>
+</style>
+
+<style>
+body {
+	background-color: #000;
+color: #000;
+}
+</style>
+</head>
+
+	<body>
+	<header>
+		<style>
+header {
+
+	text-align:center;
+}
+}
+footer    {
+	background: #009AAF;
+	color:#FFF;
+	padding: 10px;
+
+}
+a{
+	background-color: #000000;
+	color: #7ff0ff;
+	font-family: "Arial" , san-serif;
+	font-size:20px;
+	padding: 6px;
+	text-decoration:none;
+
+	border-style:solid;
+	border-color: #FC0 #00f;
+	border-width:5px;
+}
+a:hover{
+	border-color: #000 #fff;
+	color: #F0f;
+}
+</style>
+		<h3>
+		<?php
+			$i = 0;
+			for($i = 0;$i<100;$i++){
+				echo "<a href = \"tas.tritech.io/get_trolled.gif\"> something </a>";
+				if($i==13){
+					echo "<a href = \"http://tas.tritech.io/stuff_to_use/\"> stuff </a>"
+					echo "<a href = \"http://tas.tritech.io/cat\"> cat</a>"
+				}
+			}
+		?>
+		<a href = ""> something </a>
+		<a href = "http://tas.tritech.io/stuff_to_use/"> stuff </a>
+		<a href = "http://tas.tritech.io/cat"> cat</a>
+
+		</h3>
+			<h3>
+
+
+		</h3>
+		<h1></h1>
+		<h2></h2>
+		<h3></h3>
+		<h4></h4>
+			<h5></h5>
+			<h6></h6>
+			<h7></h7>
+		<p></p>
+	</header>
+	</body>
+
+</html>
